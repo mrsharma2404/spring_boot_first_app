ea6ac57a00bf4dfc693443897de12d5ba19fe6c1
mrsharma2404 <mrsharma2404@egmail.com>
2025-01-01 03:15:37 +0530

    created a seed file, but it's not working so manually added data mentioned in seed

---
+++ src/main/resources/db/seed/user_data.sql
@@ -0,0 +1,4 @@
+-- INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
+-- INSERT INTO users (name, email) VALUES ('Jane Doe', 'jane.doe@example.com');
+-- INSERT INTO users (name, email) VALUES ('Alice Smith', 'alice.smith@example.com');
+-- INSERT INTO users (name, email) VALUES ('Bob Brown', 'bob.brown@example.com');
